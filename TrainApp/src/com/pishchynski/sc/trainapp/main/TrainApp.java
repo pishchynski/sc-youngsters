8041a32433de12c11367a50c4f252bf95f94c5d3
pishchynski <pavel.pishchynski@softclub.by>
2016-07-08 16:16:13 +0300

    Created all classes for TrainApp

--- TrainApp/src/com/pishchynski/sc/trainapp/main/TrainApp.java
+++ TrainApp/src/com/pishchynski/sc/trainapp/main/TrainApp.java
@@ -1,5 +1,20 @@
 package com.pishchynski.sc.trainapp.main;
 
+import com.pishchynski.sc.trainapp.utils.Carriage;
+import com.pishchynski.sc.trainapp.utils.Train;
+import com.pishchynski.sc.trainapp.utils.TrainAppUtils;
+
+import java.util.HashMap;
+
 public class TrainApp {
 
+    private HashMap<TrainAppUtils.CarriageType, Integer> carriagePrices;
+
+    public static void main(String[] args) {
+        new TrainApp().run();
+    }
+
+    private void run() {
+
+    }
 }
